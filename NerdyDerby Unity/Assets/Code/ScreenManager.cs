f59ff47974993193322e4cc591e6ca9de71b77ae
Nicolas Castro <nicolas.castro@facens.br>
2020-11-06 16:29:17 -0300

    unity
    FirstCommit

---
+++ NerdyDerby Unity/Assets/Code/ScreenManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenManager : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject RankingPanel;
+    public GameObject RegisterPanel;
+    public GameObject RacePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ESC_Pressed();
+        }
+    }
+
+    public void RacePanel_Click()
+    {
+        RacePanel.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+    public void Ranking_Click()
+    {
+        RankingPanel.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+    public void Register_Click()
+    {
+        RegisterPanel.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+    public void ESC_Pressed()
+    {
+        RacePanel.SetActive(false);
+        RegisterPanel.SetActive(false);
+        RankingPanel.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+}
