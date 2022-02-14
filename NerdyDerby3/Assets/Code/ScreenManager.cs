ae43325cb53873f488e2722c7886aa01ba625d38
nicolas.castro <nicolas.castro@facens.br>
2022-02-14 20:35:29 -0300

    atualizacao 2022

--- NerdyDerby3/Assets/Code/ScreenManager.cs
+++ NerdyDerby3/Assets/Code/ScreenManager.cs
@@ -10,6 +10,7 @@ public class ScreenManager : MonoBehaviour
     public GameObject RegisterPanel;
     public GameObject RacePanel;
     public GameObject SettingsPanel;
+    public GameObject resultsPanel;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +22,15 @@ public class ScreenManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            ESC_Pressed();
+            if (!resultsPanel.activeSelf)
+            {
+                ESC_Pressed();
+            }
+            
         }
         if (Input.GetKeyDown(KeyCode.F1))
         {
+            
             F1_Press();
         }
     }
